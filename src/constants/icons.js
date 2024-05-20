2496eaef110e4c0293f5326aee0c846cab056e12
Rijo Sebastian <rijobdk@gmail.com>
2024-05-20 18:09:25 +0530

    Add footer

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -7,11 +7,15 @@ import {
   TrashIcon
 } from "@heroicons/react/24/outline";
 
+ 
+
 export const Icons = {
   Cart: ShoppingBagIcon,
   Bars3: Bars3Icon,
   XMark: XMarkIcon,
   Plus: PlusIcon,
   Minus: MinusIcon,
-  Trash: TrashIcon
+  Trash: TrashIcon,
+ 
+
 };
